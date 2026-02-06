61505b219ccf939e8c96ad11b2cb1e6c708b8dfe
Jonathan Putan <jonathanputan@JonathanPs-MacBook-Air.local>
2026-02-06 16:12:21 +0800

    initial commit

--- llm-python/app/config.py
+++ llm-python/app/config.py
@@ -7,7 +7,7 @@ class Settings:
     OLLAMA_BASE_URL: str = os.getenv("OLLAMA_BASE_URL", "https://ollama.com")
     OLLAMA_MODEL: str = os.getenv("OLLAMA_MODEL", "gemma3:4b")
     OLLAMA_TEMPERATURE: float = float(os.getenv("OLLAMA_TEMPERATURE", "0.7"))
-    OLLAMA_API_KEY: str = os.getenv("OLLAMA_API_KEY", "")
+    OLLAMA_API_KEY: str = os.getenv("OLLAMA_API_KEY", "c2f916de66c04c729cf98a25c93abe56._U5Dy3DoDf1RQ9BG936-yC4R")
 
 
 settings = Settings()
